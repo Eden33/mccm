186c94618c0c0379f28a1d6e6eba9a84fdbc0ca8
Eden33 <eduard.gopp@gmail.com>
2012-02-08 18:10:06 +0000

    wordpress set up with wp-config.php and page structure created.
    two dumps added. dump of fresh install and after page structure created successfully.
    
    page structure:
    
    - home
    
    - verein
    -- history
    -- vorstand
    -- mitglieder
    -- mitgliedsanmeldung
    -- kontakt
    
    - rennen
    -- anfahrtsplan
    -- strecke
    -- eintrittspreise
    -- ausschreibungen
    -- rennfahreranmeldung
    
    - archiv
    -- zeitungsartikel
    -- saison 2011 bis 2006
    
    - sponsoren
    - links
    - impressum
    
    git-svn-id: svn://localhost/websites/mccm_wordpress/trunk@3 553ebd90-c899-744e-a329-2f02278a15cf

--- wp-config.php
+++ wp-config.php
@@ -16,13 +16,13 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'database_name_here');
+define('DB_NAME', 'mccm');
 
 /** MySQL database username */
-define('DB_USER', 'username_here');
+define('DB_USER', 'mccm');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'password_here');
+define('DB_PASSWORD', 'mccm2012');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
@@ -31,7 +31,7 @@ define('DB_HOST', 'localhost');
 define('DB_CHARSET', 'utf8');
 
 /** The Database Collate type. Don't change this if in doubt. */
-define('DB_COLLATE', '');
+define('DB_COLLATE', 'utf8_general_ci');
 
 /**#@+
  * Authentication Unique Keys and Salts.
