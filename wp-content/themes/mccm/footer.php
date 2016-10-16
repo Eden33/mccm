5238cd27621c1e1fd71e07581d14f746438a22fa
Eden33 <eduard.gopp@gmail.com>
2016-10-16 09:51:23 +0000

    - set new registration start date to 2020 - remove phone number from footer - style css changes: -- proper formatting of input[mail]; input has been added to online registration forms -- add background image css for weiss-schneider and bitsche-bertschi links -- removedshared css of history- and links-page (as history page has been revised) - add new media uploaded by Andi, etc.
    git-svn-id: svn://localhost/websites/mccm_wordpress/trunk@103 553ebd90-c899-744e-a329-2f02278a15cf

--- wp-content/themes/mccm/footer.php
+++ wp-content/themes/mccm/footer.php
@@ -1,40 +1,1 @@
-<?php
-/**
- * The template for displaying the footer.
- *
- * Contains the closing of the id=main div and all content after
- *
- * @package WordPress
- * @subpackage Twenty_Eleven
- * @since Twenty Eleven 1.0
- */
-?>
-
-	</div><!-- #main -->
-
-	<footer id="colophon" role="contentinfo">
-
-			<?php
-				/* A sidebar in the footer? Yep. You can can customize
-				 * your footer with three columns of widgets.
-				 */
-				if ( ! is_404() )
-					get_sidebar( 'footer' );
-			?>
-
-			<div id="mccm-site-generator">
-				<ul>
-					<li>&copy; <?= date("Y"); ?> MCCM&nbsp;&nbsp;&nbsp;&nbsp;|</li>
-					<li>Schillerstraße 4, 6800 Feldkirch&nbsp;&nbsp;&nbsp;&nbsp;|</li>
-					<li>Tel +43 5522 79259&nbsp;&nbsp;&nbsp;&nbsp;|</li>
-					<li>Fax +43 5522 31737&nbsp;&nbsp;&nbsp;&nbsp;|</li>
-					<li><a style="color:#fff; font-style:italic; " href="mailto:zentrale@mccm-feldkirch.at">ZENTRALE@MCCM-FELDKIRCH.AT</a></li>
-				</ul>
-			</div>
-	</footer><!-- #colophon -->
-</div><!-- #page -->
-
-<?php wp_footer(); ?>
-
-</body>
-</html>
+<?php/** * The template for displaying the footer. * * Contains the closing of the id=main div and all content after * * @package WordPress * @subpackage Twenty_Eleven * @since Twenty Eleven 1.0 */?>	</div><!-- #main -->	<footer id="colophon" role="contentinfo">			<?php				/* A sidebar in the footer? Yep. You can can customize				 * your footer with three columns of widgets.				 */				if ( ! is_404() )					get_sidebar( 'footer' );			?>			<div id="mccm-site-generator">				<ul>					<li>&copy; <?= date("Y"); ?> MCCM&nbsp;&nbsp;&nbsp;&nbsp;|</li>					<li>Schillerstraße 4, 6800 Feldkirch&nbsp;&nbsp;&nbsp;&nbsp;|</li>  					<li>Fax +43 5522 31737&nbsp;&nbsp;&nbsp;&nbsp;|</li>					<li><a style="color:#fff; font-style:italic; " href="mailto:zentrale@mccm-feldkirch.at">ZENTRALE@MCCM-FELDKIRCH.AT</a></li>				</ul>			</div>	</footer><!-- #colophon --></div><!-- #page --><?php wp_footer(); ?></body></html>
