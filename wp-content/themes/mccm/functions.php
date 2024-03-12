9d46f7874b951eeb6057c4d22f491389380ca368
Eden33 <eduard.gopp@gmail.com>
2024-03-12 22:07:05 +0100

    Enable all categories for 2024

--- wp-content/themes/mccm/functions.php
+++ wp-content/themes/mccm/functions.php
@@ -79,16 +79,18 @@ function filter_the_content( $content )
     
 	if((is_page( 'mx-online-anmeldung' )
 	|| is_page( 'sjmcc-online-anmeldung')
-	|| is_page( 'oldtimer-seitenwagen-online-anmeldung' ))
+	|| is_page( 'oldtimer-seitenwagen-online-anmeldung' )
+    || is_page( 'sam-seitenwagen-online-anmeldung' )
+    || is_page( 'sam-masters-online-anmeldung' ) 
+    || is_page( 'sam-women-online-anmeldung' )
+    || is_page( 'sam-quad-online-anmeldung' ))
 	&& is_registration_enabled() === false) 
     {
         return $registration_prohibited_msg;
 	}
 	
 	// not available in 2020
-	if(is_page( 'inter-sam-online-anmeldung' )
-	    || is_page( 'sam-masters-online-anmeldung')
-	    || is_page( 'sam-junioren-open-online-anmeldung' ))
+	if( is_page( 'sam-junioren-open-online-anmeldung' ))
 	{
 	    return $registration_prohibited_msg;
 	}
