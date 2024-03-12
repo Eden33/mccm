c2e7398a8e252c51d93c93e2446d60020b788e74
Eden33 <eduard.gopp@gmail.com>
2024-03-12 19:38:59 +0100

    enable registration countdown

--- wp-content/themes/mccm/functions.php
+++ wp-content/themes/mccm/functions.php
@@ -38,10 +38,9 @@ add_action('wp_head', 'wp_head_event');
 /* REGISTER COUNTDOWN SECTION ------------------------------------------------------------------ */
 // russmedia server setting is UTC+0 
 // hour and minute configuration
-$_registration_start_date = new DateTime('2029-04-02 22:00');
-$_registration_ctr_enabled = false;
+$_registration_start_date = new DateTime('2024-03-31 22:00');
+$_registration_ctr_enabled = true;
 $_registration_ip_whitelist = array(
-	'194.208.181.177'
 );
 
 function head_menu_inject_registration_countdown($items) 
